b12047745187aac2e8fbbddabb2e72fa5a12e18c
Rafael Gogge <rafa.gogge@gmail.com>
2026-03-24 09:08:07 -0300

    alter settings for production and add search form to template.

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'recipes/home.html')
 
+def home(request):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otávio',
+    })
